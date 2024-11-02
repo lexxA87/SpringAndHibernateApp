841e725fcd8b1c6295a6944adaaf9b58e6c38711
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-11-02 12:57:22 +0400

    Added PersonController and views

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -1,15 +1,31 @@
 package org.example.dao;
 
+
 import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
 public class PersonDAO {
 
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
     public List<Person> index() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("select p from Person p", Person.class)
+                .getResultList();
     }
 
     public Person show(int id) {
